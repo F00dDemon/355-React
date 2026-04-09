3cad83c5f89fd8ada40b34bb04bdccc2d88acb92
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-04-09 10:43:54 -0700

    Done w rendering multiple cards

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,13 @@ import FunFactCard from './FunFactCard';
 function App() {
   return (
     <div>
-      <FunFactCard fact = "I have visited more than 10 countries." />
+      <h1> Fun Facts</h1>
+      <FunFactCard fact = "I have visited more than 3 countries." />
+      <FunFactCard fact = "My favorite hobby is sleeping" />
+      <FunFactCard fact = "My partner has visited more than 5 countries." />
+      <FunFactCard fact = "My partner loves rice" />
+      <FunFactCard fact = "Our favorite artisits are Nicki Minaj, Lana Del Rey, Glaive" />
+
     </div>
   );
 }
