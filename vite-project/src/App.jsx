5d098df149a861400cad1e17920d7dd86168f9b1
f00d <delossantos.orion@student.greenriver.edu>
2026-04-09 11:06:32 -0700

    styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div>
       <h1> Fun Facts</h1>
-      <FunFactCard fact = "I have visited more than 3 countries." />
+      <FunFactCard fact = "I have visited 3 countries." />
       <FunFactCard fact = "My favorite hobby is sleeping" />
-      <FunFactCard fact = "My partner has visited more than 5 countries." />
+      <FunFactCard fact = "My partner has visited 5 countries." />
       <FunFactCard fact = "My partner loves rice" />
       <FunFactCard fact = "Our favorite artisits are Nicki Minaj, Lana Del Rey, Glaive" />
 
