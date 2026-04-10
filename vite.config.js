1a684c2bd073495cc0fd80695a7ddabdc41f673c
f00d <delossantos.orion@student.greenriver.edu>
2026-04-10 10:31:17 -0700

    structure

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD:vite.config.js
   base: '355-React'
-=======
-  base: '/<your-repository-name>/'
->>>>>>> d38e07817f2a9755a08e7fb13957da9efc1559f9:vite-project/vite.config.js
 })
